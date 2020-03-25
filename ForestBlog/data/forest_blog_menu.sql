930168502b0d072e0b1bf50b80e7fe363302b6cc
Jianqi <a1752399@student.adelaide.edu.au>
2020-03-25 13:00:53 +1030

    Add a package

---
+++ ForestBlog/data/forest_blog_menu.sql
@@ -0,0 +1,5 @@
+INSERT INTO forest_blog.menu (menu_name, menu_url, menu_level, menu_icon, menu_order) VALUES ('留言板', '/message', 2, 'fa fa-comment', 1);
+INSERT INTO forest_blog.menu (menu_name, menu_url, menu_level, menu_icon, menu_order) VALUES ('LeetCode', 'https://leetcode.com/problemset/all', 2, 'ssss', 3);
+INSERT INTO forest_blog.menu (menu_name, menu_url, menu_level, menu_icon, menu_order) VALUES ('关于本站', '/aboutSite', 1, 'fa fa-info', 1);
+INSERT INTO forest_blog.menu (menu_name, menu_url, menu_level, menu_icon, menu_order) VALUES ('文章归档', '/articleFile', 1, 'fa-list-alt fa', 2);
+INSERT INTO forest_blog.menu (menu_name, menu_url, menu_level, menu_icon, menu_order) VALUES ('申请友链', '/applyLink', 1, 'fa fa-link', 3);
